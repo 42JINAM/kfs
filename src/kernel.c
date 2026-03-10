f6b20a4450d9b3511dd607484130d7303e48593a
yoonsele <yoonsele@student.42.fr>
2026-03-10 17:02:01 +0100

    write test_printf

--- src/kernel.c
+++ src/kernel.c
@@ -13,14 +13,46 @@
 //	- No any (?) interface for userside (malloc, printf, fopen, ...)
 //
 
-void	kernel_main(void)
-{
-	terminal_initialize();
-	terminal_write_line("42 hello\n");
-	terminal_write_line("42 bye\n");
+void test_printf(void) {
+	int a = 1;
+	int b = 2;
 
+	char c = 'c';
+	char d = 'd';
 	printf("printf c: %c\n", 'c');
 	printf("printf s: %s\n", "string");
 	
+	printf("int a ==========\n");
+	printf("printf p: %p \n", a);
+	printf("printf x: %x \n", a);
+	printf("printf X: %X \n", a);
+		
+	printf("int b ==========\n");
+	printf("printf p: %p \n", b);
+	printf("printf x: %x \n", b);
+	printf("printf X: %X \n", b);
+
+	printf("char c ==========\n");
+	printf("printf p: %p \n", c);
+	printf("printf x: %x \n", c);
+	printf("printf X: %X \n", c);
+	
+	printf("char d ==========\n");
+	printf("printf p: %p \n", d);
+	printf("printf x: %x \n", d);
+	printf("printf X: %X \n", d);
+
+}
+
+void	kernel_main(void)
+{
+
+
+	terminal_initialize();
+	terminal_write_line("42 hello\n");
+	terminal_write_line("42 bye\n");
+	test_printf();
+
 	keyboard_handler();
+
 }
